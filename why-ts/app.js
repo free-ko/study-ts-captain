ac40399d1b33d8de033d1f17449efa8e8d3d10e8
kyw <[duddnr654@naver.com]>
2021-01-30 12:49:48 +0900

    first commit

--- why-ts/app.js
+++ why-ts/app.js
@@ -1,21 +1,48 @@
 // api url
-var url = 'https://jsonplaceholder.typicode.com/users/1';
+const url = 'https://jsonplaceholder.typicode.com/users/1';
 
 // dom
-var username = document.querySelector('#username');
-var email = document.querySelector('#email');
-var address = document.querySelector('#address');
+let username = document.querySelector('#username');
+let email = document.querySelector('#email');
+let address = document.querySelector('#address');
 
 // user data
-var user = {};
+let user = {};
+
+/**
+ * @typedef {object} Address
+ * @property {string} street
+ * @property {string} city
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Address} address
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+
+function fetchUser() {
+  return axios.get(url);
+}
 
 function startApp() {
-  axios
-    .get(url)
+  // axios
+  //   .get(url)
+  fetchUser()
     .then(function (response) {
-      console.log(response);
+      // console.log(response);
       user = response.data;
       // TODO: 이름, 이메일, 주소 표시하기
+      username.innerText = user.name;
+      email.innerText = user.email;
+      address.innerText = user.address.street;
+      // address.innerText = user.address;   [object, object] 뜸
+      // address.innerText = user.addres;    undefined 뜸 (s빼먹어서)
     })
     .catch(function (error) {
       console.log(error);
@@ -23,3 +50,8 @@ function startApp() {
 }
 
 startApp();
+
+
+
+// 왜 변수 타입을 var에서 const로 변경하니깐 데이터 호출을 하지 못하는 거지????
+// let으로 변경하니 가져오는데...
