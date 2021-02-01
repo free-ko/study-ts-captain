f18623f90ad4a0e65814a75cd5130a0674fa9d61
kyw <[duddnr654@naver.com]>
2021-02-01 21:33:41 +0900

    인터페이스

---
+++ class-note/3_interface.ts
@@ -0,0 +1,73 @@
+interface User {
+    age: number;
+    name: string;
+}
+
+// 변수에 활용한 인터페이스
+const free: User = {
+    age: 33,
+    name: '세호'
+};
+
+// 함수에 인터페이스 활용
+function getUser(user: User) {
+    console.log(user)
+}
+
+const capt = {
+    age: 30,
+    name: '캡틴'
+}
+getUser(capt);
+
+
+// 함수의 스펙(구조)에 인터페이스를 활용
+interface SumFunction {
+    (a: number, b: number): number;
+}
+
+let sum: SumFunction;
+sum = function(a: number, b: number): number {
+    return a + b;
+}
+
+// 인덱싱
+interface StringArray {
+    [index: number]: string;
+}
+
+const arr: StringArray = ['a', 'b', 'c'];
+// arr[0] = 10 
+
+// 딕셔너리 패턴
+interface StringRegexDictionary {
+    [key: string]: RegExp;
+}
+
+const obj: StringRegexDictionary = {
+    sth: /abc/,
+    cssFile: /\.css$/,
+}
+
+// obj['cssFile'] = 'a'
+
+Object.keys(obj).forEach(function(value) {
+
+})
+
+// 인터페이스 확장
+
+interface Person {
+    name: string;
+    age: number;
+}
+
+interface Developer extends Person{
+    language: string;
+}
+
+const captain: Developer = {
+    language: 'ts',
+    age: 100,
+    name: '캡틴'
+}
