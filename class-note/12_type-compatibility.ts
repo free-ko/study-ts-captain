6a80aa2dc9981c0ecfc1a15124c8b56511541cfd
kyw <[duddnr654@naver.com]>
2021-02-11 18:22:09 +0900

    타입 호환 예제 - 함수, 제네릭

--- class-note/12_type-compatibility.ts
+++ class-note/12_type-compatibility.ts
@@ -15,3 +15,32 @@ let person: Person;
 developer = person;
 
 person = developer;   // person이 developer에 속할 수 있기 때문에 에러가 발생하지 않음
+
+
+// 함수
+let add = function(a: number) {
+    // ...
+}
+
+let sum = function(a: number, b: number) {
+    // ...
+}
+sum = add;
+// add = sum;
+
+
+// 제네릭
+interface Empty<T> {
+    // ...
+}
+
+let empty1: Empty<string>;
+
+let empty2: NoEmpty<number>;
+
+interface NoEmpty<T> {
+    data: T;
+}
+
+empty1 = empty2
+empty2 = empty1
