bb047eb98a13035c5eff0b7ee39d6bd09256d7ed
kyw <[duddnr654@naver.com]>
2021-02-11 17:31:31 +0900

    주요 메서드(조회) 타입 정의 및 실습 마무리

--- quiz/2_address-book/src/index.ts
+++ quiz/2_address-book/src/index.ts
@@ -94,11 +94,11 @@ class AddressBook {
     this.contacts.push(contact);
   }
 
-  displayListByName() {
+  displayListByName(): string[] {
     return this.contacts.map(contact => contact.name);
   }
 
-  displayListByAddress() {
+  displayListByAddress(): string[] {
     return this.contacts.map(contact => contact.address);
   }
   /* ------------------------------------------------ */
